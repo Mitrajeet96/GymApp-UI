a4f875c766cccc6277585041865a24a5293b0c0a
mitrajeetdutta <mitrajeet.dutta@embee.co.in>
2023-04-19 10:58:04 +0530

    Designing almost done, need to fix height

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgToastModule } from 'ng-angular-popup';
+import { NgConfirmModule } from 'ng-confirm-box';
+import { RegistrationComponent } from './registration/registration.component';
+import { RegistrationListComponent } from './registration-list/registration-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    RegistrationListComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+
+    NgToastModule,
+    NgConfirmModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatNativeDateModule,
+    MatDatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/registration/registration.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.scss']
+})
+export class RegistrationComponent implements OnInit {
+  public trainerOptions = ['Yes', 'No'];  //Type not defined
+  public genders = ["Male", 'Female']; //Type not defined and both '' and "" works
+  public packages = ["Monthly", "Quarterly", "Yearly"] //Type not defined
+  public importantList : string[] = [   // type defined and is best practice
+    "Toxic Fat Reduction",
+    "Energy and Endurance",
+    "Building Lean Muscles",
+    "Healthier Digestive System",
+    "Sugar Craving Body",
+    "Physical Fitness"
+  ]
+
+  public registrationForm!:FormGroup ; //! =? otherwise error
+
+  constructor(private fb:FormBuilder) {
+
+   }
+
+  ngOnInit(): void {
+    this.registrationForm = this.fb.group({
+      firstName:[''],
+      lasttName:[''],
+      email:[''],
+      mobile:[''],
+      weight:[''],
+      height:[''],
+      bmi:[''],
+      bmiResult:[''],
+      gender:[''],
+      trainerOption:[''],
+      package:[''],
+      important:[''],
+      gymHistory:[''],
+      enquiryDate:['']
+    })
+  }
+
+
+  submitRegistration(){
+    console.log(this.registrationForm.value)
+  }
+
+
+}
